ed873ca5a96c6198f011aada9f8e0058d9d0cfc8
Khushi Tyagi <khushityagi715@gmail.com>
2026-02-25 20:29:03 +0530

    Added root route

--- server/index.js
+++ server/index.js
@@ -18,6 +18,10 @@ app.use(cors({
 app.use(express.json())
 app.use(cookieParser())
 
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
 app.use("/api/auth" , authRouter)
 app.use("/api/user", userRouter)
 app.use("/api/interview" , interviewRouter)
